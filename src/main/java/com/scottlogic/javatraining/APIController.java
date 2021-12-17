44941d45d503663e8eaa9523a18a33eae1f65c54
L Franchino <lfranchino@scottlogic.com>
2021-12-17 16:58:19 +0000

    Rest API functionality integrated

--- src/main/java/com/scottlogic/javatraining/APIController.java
+++ src/main/java/com/scottlogic/javatraining/APIController.java
@@ -1,22 +1,55 @@
 package com.scottlogic.javatraining;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-import
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @RestController
 public class APIController {
 
+    Matcher matcher = new Matcher();
+
     @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
     public String helloWorld() {
         return "Hello World";
     }
 
-//    @GetMapping("/Orders")
-//    public Array getOrders() {
-//        return List<List<Order>> orderArray = new
-//    }
+    @GetMapping("/orders")
+    @ResponseStatus(HttpStatus.OK)
+    public HashMap<String, ArrayList<? extends Order>> getOrders() {
+        HashMap<String, ArrayList<? extends Order>> orderMap = new HashMap<>();
+        orderMap.put("buy", matcher.buyList);
+        orderMap.put("sell", matcher.sellList);
+        return orderMap;
+    }
+
+    @GetMapping("/pairedorders")
+    @ResponseStatus(HttpStatus.OK)
+    public ArrayList<OrderPair> getPairedOrders() {
+        return matcher.pairedOrders;
+    }
+
+    @GetMapping("/aggregates")
+    @ResponseStatus(HttpStatus.OK)
+    public HashMap<String, ArrayList<? extends AggregateOrder>> getAggregates() {
+        HashMap<String, ArrayList<? extends AggregateOrder>> aggregateMap = new HashMap<>();
+        aggregateMap.put("buy", matcher.buyAggregates);
+        aggregateMap.put("sell", matcher.sellAggregates);
+        return aggregateMap;
+    }
+
+    @PostMapping("/sellorder")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createSellOrder(@RequestBody SellOrder order) {
+        matcher.matchNewOrder(order);
+    }
+
+    @PostMapping("/buyorder")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createBuyOrder(@RequestBody BuyOrder order) {
+        matcher.matchNewOrder(order);
+    }
 }
